00d9839b8fc792c75a280c851982d94c6cf51677
ldcostanzo <lauradicostanzo98@gmail.com>
2025-03-29 19:16:53 +0100

    read.me update

--- bot.py
+++ bot.py
@@ -1,51 +1,78 @@
-import os
-from dotenv import load_dotenv
+from typing import Final
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+import os
+from dotenv import load_dotenv
 
 # Caricamento variabili ambiente
 load_dotenv()
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN: Final = os.getenv("BOT_TOKEN")
+BOT_USERNAME: Final = os.getenv('BOT_USER')
 URL = os.getenv("URL")
 
-if not TOKEN:
-    raise ValueError("Il token del bot non è stato trovato. Assicurati che il file .env sia corretto.")
+async def start_command(update: Update,context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Hello Laura i\'m your test bot')
 
-# Crea l'applicazione del bot
-app = Application.builder().token(TOKEN).build()
+async def help_command(update: Update,context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Mi scoccio di scrivere l help ')
 
-# Comandi
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Ciao! Sono il tuo bot Telegram.")
-    chat_id = update.effective_message.chat_id
-    print(chat_id)
+async def custom_command(update: Update,context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Inserisci il comando custom')
 
-async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Ecco i comandi disponibili:\n/start - Avvia il bot\n/help - Mostra aiuto")
+def handle_response(text: str) -> str:
+    processed: str = text.lower()
+
+    if 'ciao' in processed:
+        return 'ciao'
+
+    if 'come stai' in processed:
+        return 'bene grazie, tu?'
+
+    return 'Bro'
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text_message = update.message.text.lower()
-    response = "Non ho capito la domanda, bro, parla potabile"
-    if "ciao" in text_message:
-        response = "Ciao anche a te bro"
-    elif "come stai" in text_message:
-        response = "Bene, grazie! Tu?"
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+
+    print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
+
+    if message_type == 'group' or message_type == 'supergroup':
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME,'').strip()
+            response: str = handle_response(new_text)
+        else:
+            return
+    else:
+        response: str = handle_response(text)
+    print('Bot',response)
     await update.message.reply_text(response)
 
-# Aggiunta handler
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("help", help))
-app.add_handler(MessageHandler(filters.TEXT, handle_message))
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
 
-# Avvia il bot in modalità webhook
-if __name__ == "__main__":
-    print("Avvio del bot...")
-    app.run_webhook(
-        listen="0.0.0.0",
-        port=443,
-        url_path=TOKEN,  # URL locale del webhook
-        webhook_url=f"{URL}/{TOKEN}",  # URL pubblico del webhook
-        key="/certs/key.pem",
-        cert="/certs/cert.pem"
-    )
-    print(f"Webhook configurato su: {URL}/{TOKEN}")
+if __name__ == '__main__':
+    print('Start bot')
+    app =  Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler('start',start_command))
+    app.add_handler(CommandHandler('help',help_command))
+    app.add_handler(CommandHandler('custom',custom_command))
+
+    app.add_handler(MessageHandler(filters.TEXT,handle_message))
+
+    app.add_error_handler(error)
+    
+    print('Start Polling')
+    app.run_polling(poll_interval=3)
+
+#  Avvia il bot in modalità webhook
+
+#    app.run_webhook(
+#         listen="0.0.0.0",
+#         port=443,
+#         url_path=TOKEN,  # URL locale del webhook
+#         webhook_url=f"{URL}/{TOKEN}",  # URL pubblico del webhook
+#         key="/certs/key.pem",
+#         cert="/certs/cert.pem"
+#         )  
+# print(f"Webhook configurato su: {URL}/{TOKEN}")
